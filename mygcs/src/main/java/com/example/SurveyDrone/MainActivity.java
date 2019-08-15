cf474092035cedb510994fe5fb4b65061d95d91f
serahan <seyrahan22@gmail.com>
2019-08-15 18:48:27 +0900

    [UI] 소프트바, 상태바 제거 마침.

--- mygcs/src/main/java/com/example/SurveyDrone/MainActivity.java
+++ mygcs/src/main/java/com/example/SurveyDrone/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.SurveyDrone;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.o3dr.services.android.lib.drone.property.Type;
+import com.o3dr.android.client.ControlTower;
+import com.o3dr.android.client.Drone;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private int droneType = Type.TYPE_UNKNOWN;
+    private ControlTower controlTower;
+    private Drone drone;
+
+    NaverMap naverMap;
+    MapFragment mNaverMapFragment = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "Start mainActivity");
         super.onCreate(savedInstanceState);
+        // 소프트바 지우기
+        deleteStatusBar();
+        // 상태바 지우기
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        //
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+
+        final Context context = getApplicationContext();
+        this.controlTower = new ControlTower(context);
+        this.drone = new Drone(context);
+
+        FragmentManager fm = getSupportFragmentManager();
+        mNaverMapFragment = (MapFragment) fm.findFragmentById(R.id.map);
+        if (mNaverMapFragment == null) {
+            mNaverMapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map, mNaverMapFragment).commit();
+        }
+    }
+
+    private void deleteStatusBar() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE
+        );
     }
 }
