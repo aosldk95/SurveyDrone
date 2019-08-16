fd21fc42b61cc7f23fac45543f467a3d9c87ca9c
serahan <seyrahan22@gmail.com>
2019-08-16 11:52:29 +0900

    [UI] 드론 연결 기본 세팅 마침. 버튼 구현 미완성.

--- mygcs/src/main/java/com/example/SurveyDrone/MainActivity.java
+++ mygcs/src/main/java/com/example/SurveyDrone/MainActivity.java
@@ -1,32 +1,63 @@
 package com.example.SurveyDrone;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
-import com.o3dr.services.android.lib.drone.property.Type;
 import com.o3dr.android.client.ControlTower;
 import com.o3dr.android.client.Drone;
+import com.o3dr.android.client.apis.ControlApi;
+import com.o3dr.android.client.apis.VehicleApi;
+import com.o3dr.android.client.interfaces.DroneListener;
+import com.o3dr.android.client.interfaces.LinkListener;
+import com.o3dr.android.client.interfaces.TowerListener;
+import com.o3dr.services.android.lib.drone.attribute.AttributeEvent;
+import com.o3dr.services.android.lib.drone.attribute.AttributeType;
+import com.o3dr.services.android.lib.drone.property.State;
+import com.o3dr.services.android.lib.drone.property.Type;
+import com.o3dr.services.android.lib.drone.property.VehicleMode;
+import com.o3dr.services.android.lib.gcs.link.LinkConnectionStatus;
+import com.o3dr.services.android.lib.model.AbstractCommandListener;
+import com.o3dr.services.android.lib.model.SimpleCommandListener;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements DroneListener, TowerListener, LinkListener, OnMapReadyCallback {
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    MapFragment mNaverMapFragment = null;
+    LatLng ClickLatLng = null;
+
+    private Drone drone;
     private int droneType = Type.TYPE_UNKNOWN;
     private ControlTower controlTower;
-    private Drone drone;
+
+    private Spinner modeSelector;
 
     NaverMap naverMap;
-    MapFragment mNaverMapFragment = null;
+
+    private final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +74,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         this.controlTower = new ControlTower(context);
         this.drone = new Drone(context);
 
+        // 지도 띄우기
         FragmentManager fm = getSupportFragmentManager();
         mNaverMapFragment = (MapFragment) fm.findFragmentById(R.id.map);
         if (mNaverMapFragment == null) {
@@ -50,7 +82,70 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             fm.beginTransaction().add(R.id.map, mNaverMapFragment).commit();
         }
 
+        // 모드 변경 스피너
+        this.modeSelector = (Spinner) findViewById(R.id.modeSelect);
+        this.modeSelector.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE);
+                onFlightModeSelected(view);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> prent) {
+                // Do nothing
+            }
+        });
+
         mNaverMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+
+        UiSettings uiSettings = naverMap.getUiSettings();
+
+        // 줌 버튼 제거
+        uiSettings.setZoomControlEnabled(false);
+
+        // 축척 바 제거
+        uiSettings.setScaleBarEnabled(false);
+
+        // 클릭 이벤트 리스너
+        naverMap.setOnMapClickListener(new NaverMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull PointF pointF, @NonNull LatLng latLng) {
+                ClickLatLng = latLng;
+            }
+        });
+        Log.d("MapLog", "ClickLatLng : " + ClickLatLng);
+    }
+
+    public void onFlightModeSelected(View view) {
+        final VehicleMode vehicleMode = (VehicleMode) this.modeSelector.getSelectedItem();
+
+        VehicleApi.getApi(this.drone).setVehicleMode(vehicleMode, new AbstractCommandListener() {
+            @Override
+            public void onSuccess() {
+                alertUser("비행 모드 " + vehicleMode.toString() + "로 변경.");
+            }
+
+            @Override
+            public void onError(int executionError) {
+                alertUser("비행 모드 변경 실패 : " + executionError);
+            }
+
+            @Override
+            public void onTimeout() {
+                alertUser("비행 모드 변경 시간 초과.");
+            }
+        });
+    }
+
+    private void alertUser(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        Log.d(TAG, message);
     }
 
     private void deleteStatusBar() {
@@ -65,9 +160,155 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onMapReady(@NonNull NaverMap naverMap) {
-        UiSettings uiSettings = naverMap.getUiSettings();
+    public void onDroneEvent(String event, Bundle extras) {
+        switch (event) {
+            case AttributeEvent.STATE_CONNECTED:
+                alertUser("드론이 연결되었습니다.");
+                break;
 
-        uiSettings.setZoomControlEnabled(false);
+            case AttributeEvent.STATE_DISCONNECTED:
+                alertUser("드론이 연결 해제되었습니다.");
+                break;
+
+            case AttributeEvent.TYPE_UPDATED:
+                Type newDroneType = this.drone.getAttribute(AttributeType.TYPE);
+                if (newDroneType.getDroneType() != this.droneType) {
+                    this.droneType = newDroneType.getDroneType();
+                    updateVehicleModesForType(this.droneType);
+                }
+                break;
+
+            default:
+                // Log.i("DRONE_EVENT", event); //Uncomment to see events from the drone
+                break;
+        }
+    }
+
+    @Override
+    public void onDroneServiceInterrupted(String errorMsg) {
+
+    }
+
+    @Override
+    public void onLinkStateUpdated(@NonNull LinkConnectionStatus connectionStatus) {
+        switch (connectionStatus.getStatusCode()) {
+            case LinkConnectionStatus.FAILED:
+                Bundle extras = connectionStatus.getExtras();
+                String msg = null;
+                if (extras != null) {
+                    msg = extras.getString(LinkConnectionStatus.EXTRA_ERROR_MSG);
+                }
+                alertUser("연결 실패 :" + msg);
+                break;
+        }
+    }
+
+    @Override
+    public void onTowerConnected() {
+        alertUser("드론-핸드폰 연결 성공.");
+        this.controlTower.registerDrone(this.drone, this.handler);
+        this.drone.registerDroneListener(this);
+    }
+
+    @Override
+    public void onTowerDisconnected() {
+        alertUser("드론-핸드폰 연결 해제.");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.controlTower.connect(this);
+        updateVehicleModesForType(this.droneType);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (this.drone.isConnected()) {
+            this.drone.disconnect();
+        }
+
+        this.controlTower.unregisterDrone(this.drone);
+        this.controlTower.disconnect();
+    }
+
+    protected void updateVehicleModesForType(int droneType) {
+        List<VehicleMode> vehicleModes = VehicleMode.getVehicleModePerDroneType(droneType);
+        ArrayAdapter<VehicleMode> vehicleModeArrayAdapter = new ArrayAdapter<VehicleMode>(this, android.R.layout.simple_spinner_item, vehicleModes);
+        vehicleModeArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.modeSelector.setAdapter(vehicleModeArrayAdapter);
+    }
+
+    public void onArmButtonTap(View view) {
+        State vehicleState = this.drone.getAttribute(AttributeType.STATE);
+
+        if (vehicleState.isFlying()) {
+            // Land
+            VehicleApi.getApi(this.drone).setVehicleMode(VehicleMode.COPTER_LAND, new SimpleCommandListener() {
+                @Override
+                public void onError(int executionError) {
+                    alertUser("착륙할 수 없습니다 : " + executionError);
+                }
+
+                @Override
+                public void onTimeout() {
+                    alertUser("착륙할 수 없습니다.");
+                }
+            });
+        } else if (vehicleState.isArmed()) {
+            // Take off
+            ControlApi.getApi(this.drone).takeoff(3, new AbstractCommandListener() {
+                @Override
+                public void onSuccess() {
+                    alertUser("이륙에 성공하였습니다.");
+                }
+
+                @Override
+                public void onError(int executionError) {
+                    alertUser("이륙 할 수 없습니다 : " + executionError);
+                }
+
+                @Override
+                public void onTimeout() {
+                    alertUser("이륙 할 수 없습니다.");
+                }
+            });
+        } else if (!vehicleState.isConnected()) {
+            // Connect
+            alertUser("드론 연결을 하십시오.");
+        } else {
+            // Connected but not Armed
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Arming...");
+            builder.setMessage("시동을 걸면 프로펠러가 고속으로 회전합니다.");
+            builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Arming();
+                }
+            });
+            builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            builder.show();
+        }
+    }
+
+    public void Arming() {
+        VehicleApi.getApi(this.drone).arm(true, false, new SimpleCommandListener() {
+            @Override
+            public void onError(int executionError) {
+                alertUser("아밍 할 수 없습니다 " + executionError);
+            }
+
+            @Override
+            public void onTimeout() {
+                alertUser("아밍 할 수 없습니다.");
+            }
+        });
     }
 }
